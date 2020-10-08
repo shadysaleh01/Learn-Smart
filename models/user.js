2c626f6467004db5ca9d580ba2607186db457947
Monika Kakuturu <monikakakuturu@gmail.com>
2020-10-08 16:35:54 -0700

    merged the user table data into the existing User model object

--- models/user.js
+++ models/user.js
@@ -3,20 +3,35 @@ const bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", {
+    // name cannot be null
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     // The email cannot be null, and must be a proper email before creation
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true
-      }
+        isEmail: true,
+      },
     },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
+    // The default score is zero
+    score: {
+      type: DataTypes.BIGINT,
+      defaultValue: 0,
+    },
+    // squad name cannot be null
+    squad: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
@@ -24,7 +39,7 @@ module.exports = function(sequelize, DataTypes) {
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", user => {
+  User.addHook("beforeCreate", (user) => {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
