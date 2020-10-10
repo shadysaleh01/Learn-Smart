9404be987eeaae3f557c5d30a70ca222704f6079
Shady Coder <shadysaleh01@gmail.com>
2020-10-09 17:10:14 -0700

    Updated

--- public/js/play.js
+++ public/js/play.js
@@ -14,58 +14,55 @@ $(document).ready(() => {
     categoryChoice = $(this).data("category");
     console.log(categoryChoice);
     $("#cat-setting").text(categoryChoice);
-    categoryChosen(categoryChoice)
   });
 
   $("#hide-toggle").on("click", function (event) {
-    var state = $("#answers-area").data("state");
+    let state = $("#q-and-a").data("state");
 
     console.log(state);
     if (state === "showing") {
-      $("#answers-area").data("state", "hiding");
-      $("#answers-area").addClass("hide");
-      $("#clicker-area").removeClass("hide");
-      $("#clicker-area").data("state", "showing");
-      $("#question-display").html(
-        "Click the <span class='red' id='clickerSpan' style='color:white'>blue</span> button!"
-      );
+      // $("#answers-area").data("state", "hiding");
+      // $("#answers-area").addClass("hide");
+      // $("#clicker-area").removeClass("hide");
+      // $("#clicker-area").data("state", "showing");
+      // $("#question-display").html(
+      //   "Click the <span class='red' id='clickerSpan' style='color:white'>blue</span> button!"
+      // );
+      $("#q-and-a").data("state", "hiding");
+      $("#q-and-a").addClass("hide");
+      $("#cat-encap").data("state", "showing");
+      $("#cat-encap").removeClass("hide");
     } else {
-      $("#answers-area").data("state", "showing");
-      $("#answers-area").removeClass("hide");
-      $("#clicker-area").addClass("hide");
-      $("#clicker-area").data("state", "hiding");
-      $("#question-display").html(
-        "This tiny tyrant ruled across Europe at 5 feet 6 inches"
-      );
+      // $("#answers-area").data("state", "showing");
+      // $("#answers-area").removeClass("hide");
+      // $("#clicker-area").addClass("hide");
+      // $("#clicker-area").data("state", "hiding");
+      // $("#question-display").html(
+      //   "This tiny tyrant ruled across Europe at 5 feet 6 inches"
+      // );
+      $("#q-and-a").data("state", "showing");
+      $("#q-and-a").removeClass("hide");
+      $("#cat-encap").data("state", "hiding");
+      $("#cat-encap").addClass("hide");
     }
   });
 
   // hide game over overlays
-  $("#hide-toggle-game-over").on("click", function (event) {
+  $(".hide-toggle-game-over").on("click", function (event) {
     // toggle the overlays
-    $("#game-over").addClass("hide");
-    $("#overlay-background").addClass("hide");
+    let state = $("#game-over-encap").data("state");
+    console.log(state);
+    if (state === "showing") {
+      $("#game-over-encap").addClass("hide");
+      $("#game-over-encap").data("state", "hiding");
+    } else {
+      $("#game-over-encap").removeClass("hide");
+      $("#game-over-encap").data("state", "showing");
+    }
+
   });
 
 });
 
-// function categoryChosen(category) {
-
-//   $.get(`/api/questions/category/${category}`, (data) => {
-//     console.log(data)
-//     const questionsArr = data.question
-//     const answersArr = data.answer
-//     for (let i = 0; i < data.length; i++) {
-//       $("#question-display").text(data.question[0])
-//       $("#answer-1").text(data.answer)
-//       $("#answer-2").text(data.answer[i])
-//       $("#answer-3").text(data.answer[i])
-//       $("#answer-4").text(data.answer[i])
-
-//     }
-//   })
-
-
-// }
 
 
