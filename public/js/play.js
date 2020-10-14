c5af092f69379acf3598083235cea44cf900790b
jessef <1319957+jfrausto@users.noreply.github.com>
2020-10-13 22:39:37 -0700

    create game over screen pop up logic

--- public/js/play.js
+++ public/js/play.js
@@ -109,6 +109,20 @@ $(document).ready(() => {
 
   $(".answer").on("click", verifyResponse);
 
+  // event listeners for retaking quizzes
+  $("#play-this-again").on("click", startQuiz);
+  $("#play-new-cat").on("click", function(event) {
+    $("#game-over-encap").fadeOut("slow");
+    $("#game-over-encap").addClass("hide");
+    $("#game-over-encap").data("state", "hiding");
+    $("#q-and-a").data("state", "hiding");
+    $("#q-and-a").addClass("hide");
+    $("#cat-encap").data("state", "showing");
+    $("#cat-encap").removeClass("hide");
+  });
+
+
+
   function squadChosen(squad) {
     const twoValue = { squad: squad, email: localStorage.userEmail }
     $.ajax({
@@ -163,6 +177,7 @@ function verifyResponse() {
 function renderQuestion() {
   // ! game over if we run out of questions
   if (currentQuestion >= quiz10.length) {
+    currentQuestion = 0;
     console.log("GAME OVERRRRRRRRRRRRRRRRRR");
     gameOver();
     return;
@@ -212,6 +227,13 @@ function renderQuestion() {
 
 function startQuiz() {
   // ? show and hide divs stuff goes here
+  // hide game over screen
+  $("#congratulations-msg").addClass("hide");
+  $("#try-again-msg").addClass("hide");
+  $("#map-btn").addClass("hide");
+  $("#game-over-encap").fadeOut("slow");
+  $("#game-over-encap").addClass("hide");
+  $("#game-over-encap").data("state", "hiding");
   $("#q-and-a").data("state", "showing");
   $("#q-and-a").removeClass("hide");
   $("#cat-encap").data("state", "hiding");
@@ -225,15 +247,26 @@ function startQuiz() {
   // ! reset the 10 questions
   quiz10 = [];
   // ? how many questions do we want in our quiz? loop 10 times!
-  for (let k = 0; k < 9; k++) {
+  for (let k = 0; k < 10; k++) {
     quiz10.push(categoryData[k]);
   }
   renderQuestion();
 }
 
 function gameOver() {
+  $("#q-and-a").addClass("hide");
+  $("#game-over-encap").fadeIn("slow");
   $("#game-over-encap").removeClass("hide");
   $("#game-over-encap").data("state", "showing");
   // $("#user-name").text(userName);
   $("#user-score").text(score);
+
+  if(score >= 700) {
+    // reveal congrats
+    $("#congratulations-msg").removeClass("hide");
+    // reveal go to map button
+    $("#map-btn").removeClass("hide");
+  } else {  // you failed!
+    $("#try-again-msg").removeClass("hide");
+  }
 }
