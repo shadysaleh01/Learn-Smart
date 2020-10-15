5f35a0dcf4d0bc2b89835ddb1ebbf1514a9da1fd
Shady Coder <shadysaleh01@gmail.com>
2020-10-14 22:52:04 -0700

    Created icon with dropdown list in nav bar to direct user to their profile or to logout

--- public/js/home.js
+++ public/js/home.js
@@ -64,6 +64,13 @@ if (localStorage.isAuthenticated === "true") {
 
 
 // }
+document.addEventListener('DOMContentLoaded', function () {
+   var elems = document.querySelectorAll('.dropdown-trigger');
+   var options = { constrainWidth: false, coverTrigger: false, alignment: 'left', closeOnClick: false }
+   var instances = M.Dropdown.init(elems, options);
+});
+
+var instance = M.Dropdown.getInstance(elem);
 
 
 
